cb021211cc598f127d0bb4d44fe39f9c9f98c38e
Amanullah Aman <amanullah.bcse@gmail.com>
2020-01-07 23:16:00 +0600

    onlinemarketplace

--- crud/inc/footer.php
+++
@@ -1,8 +0,0 @@
- </section>
-<section class="footeroption">
-	<p>&copy; <?php echo date("Y"); ?> Training with live project</p>
-		<h2><?php echo "www.trainingwithliveproject.com"; ?></h2>
-	</section>
-</div>
-</body>
-</html>
